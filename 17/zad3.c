1ec79b04e8644b46065d55c6cc11c61d5b1685f7
Yana Zheleva <56817475+bibiyan@users.noreply.github.com>
2022-04-11 01:54:45 +0300

    Add files via upload

---
+++ 17/zad3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]){
+    if (argc != 4){
+        perror("Invalid arguments!\n");
+        return EXIT_FAILURE;
+    }
+    if (argv[2] != "&&"){
+        perror("Invalid arguments!\n");
+        return EXIT_FAILURE;
+    }
+    pid_t pid = fork();
+    if (pid == -1){
+        perror("fork problem");
+        return EXIT_FAILURE;
+    }else if (0 == pid){
+        if (execlp(argv[1], argv[1], 0) == 0){
+            perror("Error\n");
+            return EXIT_FAILURE;
+        }
+    } else {
+       if (execlp(argv[2], argv[2], 0) ==0){
+            perror("Error\n");
+            return EXIT_FAILURE;
+        }
+    }
+    return EXIT_SUCCESS;
+}
